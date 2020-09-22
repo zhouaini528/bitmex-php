65f92086162fb055002f3a6842b2c536bd5a1bc6
lin <465382251@qq.com>
2020-09-22 11:15:37 +0800

    add header

--- src/Request.php
+++ src/Request.php
@@ -14,71 +14,71 @@ class Request
      * 是否开启bitmex测试账号，需要先去申请测试账号。
      * */
     protected $key='';
-    
+
     protected $secret='';
-    
+
     protected $host='';
-    
+
     protected $nonce='';
-    
+
     protected $signature='';
-    
+
     protected $headers=[];
-    
+
     protected $type='';
-    
+
     protected $path='';
-    
+
     protected $data=[];
-    
+
     protected $options=[];
-    
+
     public function __construct(array $data)
     {
         $this->key=$data['key'] ?? '';
         $this->secret=$data['secret'] ?? '';
         $this->host=$data['host'] ?? 'https://www.bitmex.com';
-        
+
         $this->options=$data['options'] ?? [];
     }
-    
+
     /**
      * 认证
      * */
     protected function auth(){
         $this->nonce();
-        
+
         $this->signature();
-        
+
         $this->headers();
-        
+
         $this->options();
     }
-    
+
     /**
-     * 
+     *
      * */
     protected function nonce(){
         //Time delay 30 seconds
         $this->nonce=time()+30;
     }
-    
+
     /**
-     * 
+     *
      * */
     protected function signature(){
         $endata=http_build_query($this->data);
         $this->signature=hash_hmac('sha256', $this->type.$this->path.$this->nonce.$endata, $this->secret);
     }
-    
+
     /**
-     * 
+     *
      * */
     protected function headers(){
         $this->headers=[
             'accept' => 'application/json',
         ];
-        
+
         if(!empty($this->key) && !empty($this->secret)) {
             $this->headers=array_merge($this->headers,[
                 'api-expires'      => $this->nonce,
@@ -86,21 +86,19 @@ class Request
                 'api-signature' => $this->signature,
             ]);
         }
-        
+
         if(!empty($this->data)) $this->headers['content-type']='application/x-www-form-urlencoded';
     }
-    
+
     /**
-     * 
+     *
      * */
     protected function options(){
-        $this->options=array_merge([
-            'headers'=>$this->headers,
-            //'verify'=>false
-        ],$this->options);
-        
+        if(isset($this->options['headers'])) $this->headers=array_merge($this->headers,$this->options['headers']);
+
+        $this->options['headers']=$this->headers;
         $this->options['timeout'] = $this->options['timeout'] ?? 60;
-        
+
         if(isset($this->options['proxy']) && $this->options['proxy']===true) {
             $this->options['proxy']=[
                 'http'  => 'http://127.0.0.1:12333',
@@ -109,32 +107,32 @@ class Request
             ];
         }
     }
-    
+
     /**
-     * 
+     *
      * */
     protected function send(){
         $client = new \GuzzleHttp\Client();
-        
+
         if(!empty($this->data)) $this->options['form_params']=$this->data;
-        
+
         $response = $client->request($this->type, $this->host.$this->path, $this->options);
-        
+
         return $response->getBody()->getContents();
     }
-    
+
     /*
-     * 
+     *
      * */
     protected function exec(){
         $this->auth();
-        
+
         try {
             return json_decode($this->send(),true);
         }catch (RequestException $e){
             if(method_exists($e->getResponse(),'getBody')){
                 $contents=$e->getResponse()->getBody()->getContents();
-                
+
                 $temp=json_decode($contents,true);
                 if(!empty($temp)) {
                     $temp['_method']=$this->type;
@@ -145,9 +143,9 @@ class Request
             }else{
                 $temp['_message']=$e->getMessage();
             }
-            
+
             $temp['_httpcode']=$e->getCode();
-            
+
             throw new Exception(json_encode($temp));
         }
     }
